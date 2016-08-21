8bab730ae5b3b53eb582e5f71ed31850e6d90866
宁凯 <ningkaikok@163.com>
2016-08-21 21:52:45 +0800

    fix:修改formatMessage 函数

--- wechat/util.js
+++ wechat/util.js
@@ -16,28 +16,29 @@ exports.parseXMLAsync = function (xml) {
     });
 }
 
-function formatMessage(result) {
-    var message = {};
-    if (typeof result === 'object') {
-        var keys = Object.keys(result);
+function formatMessage(result){
+    var message={};
+    if(typeof result === "object"){
+        var keys = Object.keys(result)
+        // console.log(keys)
         for (var i = 0; i < keys.length; i++) {
             var item = result[keys[i]];
             var key = keys[i];
-            if (!(item instanceof Array) || item.length === 0) {
+            // 如果不是数组 或者 length 是0 跳过
+            if(!(item instanceof Array) || item.length === 0){
                 continue;
             }
-            if (item.length === 1) {
+            if(item.length === 1){
                 var val = item[0];
-                if (typeof val === 'object') {
-                    message[key] = formatMessage(val);
-                } else {
-                    message[key] === (val || '').trim()
+                if(typeof val === 'object'){
+                    message[key] = formatMessage(val)
+                }else{
+                    message[key] = (val || '').trim();
                 }
-            }
-            else {
-                message[key] = [];
-                for (var j = 0, k = item.length; j < k; j++) {
-                    message[key].push(formatMessage(item[j]));
+            }else{
+                message[key] = []
+                for (var j = 0; j < item.length; j++) {
+                    message[key].push(formatMessage(item[j]))
                 }
             }
         }
